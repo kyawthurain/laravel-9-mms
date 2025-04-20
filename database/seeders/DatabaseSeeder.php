a1e20f79405e2f8a2679e9a7c2a25370484def60
kyawthurain <kyawthurain1982002@gmail.com>
2025-04-20 19:18:28 +0630

    Revert "post and user migrations and seed"
    This reverts commit 863d58e29749e363534af9c978a91329d1115117.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,14 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => "ktr",
-            'email' => "ktr@gmail.com",
-            'password' => "123456789",
-            'role' => 'admin',
-        ]);
-
-        \App\Models\User::factory(10)->create();
-        Post::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
     }
 }
