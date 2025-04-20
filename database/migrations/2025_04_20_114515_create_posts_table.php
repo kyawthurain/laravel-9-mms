c85fe6850128e9aba8acd0ae94bb979a8150ce00
kyawthurain <kyawthurain1982002@gmail.com>
2025-04-20 19:19:20 +0630

    Reapply "post and user migrations and seed"
    This reverts commit a1e20f79405e2f8a2679e9a7c2a25370484def60.

--- database/migrations/2025_04_20_114515_create_posts_table.php
+++ database/migrations/2025_04_20_114515_create_posts_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',['admin','user','owner'])->default('user');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string("description");
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => "ktr",
+            'email' => "ktr@gmail.com",
+            'password' => "123456789",
+            'role' => 'admin',
+        ]);
+
+        \App\Models\User::factory(10)->create();
+        Post::factory(10)->create();
     }
 }
